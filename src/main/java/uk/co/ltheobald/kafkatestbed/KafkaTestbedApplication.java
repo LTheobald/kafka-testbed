eb87a758f13aae91d1ebf7a27e1f9945acb5c8c9
Lee Theobald <lee@ltheobald.co.uk>
2025-04-08 22:29:25 +0200

    Added Spring Starter

---
+++ src/main/java/uk/co/ltheobald/kafkatestbed/KafkaTestbedApplication.java
@@ -0,0 +1,13 @@
+package uk.co.ltheobald.kafkatestbed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTestbedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaTestbedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/ltheobald/kafkatestbed/KafkaTestbedApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.ltheobald.kafkatestbed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTestbedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

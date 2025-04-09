a9d3aaf7301c7dc5b03a92134de7df6cb62566ba
Lee Theobald <lee@ltheobald.co.uk>
2025-04-09 22:10:22 +0200

    Starting to add Avro

--- src/main/java/uk/co/ltheobald/kafkatestbed/Infrastructure.java
+++ src/main/java/uk/co/ltheobald/kafkatestbed/Infrastructure.java
@@ -1,10 +1,10 @@
-package myproject;
+package uk.co.ltheobald.kafkatestbed;
 
 import com.pulumi.Pulumi;
 import com.pulumi.core.Output;
 import com.pulumi.aws.s3.BucketV2;
 
-public class App {
+public class Infrastructure {
     public static void main(String[] args) {
         Pulumi.run(ctx -> {
             var bucket = new BucketV2("my-bucket");
---
+++ src/main/java/uk/co/ltheobald/kafkatestbed/services/Authorisations.java
@@ -0,0 +1,14 @@
+package uk.co.ltheobald.kafkatestbed.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+class Authorisations {
+
+    /**
+     * Create a random transaction every X seconds
+     */
+    public void createAuthorisations() {
+
+    }
+}

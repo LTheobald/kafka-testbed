871f9cbc4490ee349311c4a0564ac7a8958bd0db
Lee Theobald <lee@ltheobald.co.uk>
2025-04-10 10:46:17 +0200

    Slowly adding some Avro types and moving towards Kafka

--- src/main/java/uk/co/ltheobald/kafkatestbed/services/Authorisations.java
+++ src/main/java/uk/co/ltheobald/kafkatestbed/services/Authorisations.java
@@ -1,14 +1,31 @@
 package uk.co.ltheobald.kafkatestbed.services;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import uk.co.ltheobald.kafkatestbed.Transaction;
+
+import java.time.Instant;
+import java.util.UUID;
+import java.util.random.RandomGenerator;
 
 @Service
 class Authorisations {
+    private static final double MIN_AMOUNT = 0.01;
+    private static final double MAX_AMOUNT = 150.00;
+    private static final RandomGenerator RANDOM_GENERATOR = RandomGenerator.of("L64X256MixRandom");
+
 
     /**
      * Create a random transaction every X seconds
      */
+    @Scheduled(fixedRate = 2000)
     public void createAuthorisations() {
-
+        Transaction tx = Transaction.newBuilder()
+                .setAmount(MIN_AMOUNT + RANDOM_GENERATOR.nextDouble() * (MAX_AMOUNT - MIN_AMOUNT))
+                .setCurrency("GBP")
+                .setMerchantId("merchant-1")
+                .setTransactionId(UUID.randomUUID())
+                .setTimestamp(Instant.now())
+                .build();
     }
 }
